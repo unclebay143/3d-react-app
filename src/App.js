6242690c0f0291f52722b5da50056ffd5603c63f
unclebigbay <ayodelesamueladebayo143@gmail.com>
2022-08-03 19:17:04 +0100

    Ready to 🚀

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import Cylinder3d from "./component/Cylinder3d";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className='App-header'>
+        {/* Canvas 1 */}
+        <Canvas>
+          <pointLight position={[10, 10, 10]} />
+          <ambientLight />
+          <Cylinder3d position={[-1.2, 0, 0]} />
+          <Cylinder3d position={[1.2, 0, 0]} />
+        </Canvas>
+
+        {/* Canvas 2 */}
+        <Canvas>
+          <pointLight position={[10, 10, 10]} />
+          <ambientLight intensity={0.5} />
+          <Cylinder3d position={[-1.2, 0, 0]} wireframe={true} />
+          <Cylinder3d position={[1.2, 0, 0]} wireframe={true} />
+        </Canvas>
+
+        {/* Canvas 3 */}
+        <Canvas>
+          <pointLight position={[10, 10, 10]} />
+          <ambientLight color={"red"} />
+          <Cylinder3d position={[-1.2, 0, 0]} />
+          <Cylinder3d position={[1.2, 0, 0]} />
+        </Canvas>
+      </section>
+    </>
   );
 }
 
